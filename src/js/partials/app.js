8881c36662ebe7ebe9ec75b76a787904edaa44c4
Ruslan Polonkoev <suslik87@gmail.com>
2015-06-30 12:42:31 +0300

    added link to latest news

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -12,5 +12,9 @@ $(function(){
     $('.up-button').on('click', function(e){
         e.preventDefault();
         $('body').animate({scrollTop:0}, 500, 'swing');
-    })
+    });
+
+    $('.container').on('click', 'a', function(e){
+        e.preventDefault();
+    });
 });
